11af3be16df4c873c7a7998e4bac6b59ddfaedbf
Dennis Peters <dennis95peters@gmail.com>
2021-05-11 02:39:41 +0300

    This commit implements the API exceptions

--- src/main/java/Exceptions/ApiExceptions.java
+++ src/main/java/Exceptions/ApiExceptions.java
@@ -1,4 +1,16 @@
 package Exceptions;
 
-public class ApiExceptions {
+public class ApiExceptions extends RuntimeException{
+
+    private final int statusCode;
+
+    public ApiExceptions (int statusCode, String msg){
+        super(msg); //see explanation below
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
 }
+
